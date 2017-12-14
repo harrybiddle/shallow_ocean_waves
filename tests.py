b16359513823445ff0c8f2dc79a2903ed81a30ee
Harry Biddle <noemail@domain.com>
2017-12-14 09:34:26 +0100

    fix test of dh_dx term

--- tests.py
+++ tests.py
@@ -23,43 +23,6 @@ class NumpyTestCase(unittest.TestCase):
     def assert_arrays_unequal(self, a, b):
         self.assertFalse(np.array_equal(a, b))
 
-# class TestAdaptiveTimeStepper(unittest.TestCase):
-
-#     def setUp(self):
-#         self.u = np.arange(25).reshape((5, 5))
-#         self.v = np.arange(25).reshape((5, 5))
-#         self.h = np.arange(25).reshape((5, 5))
-#         self.timestep = mock.Mock()
-#         self.starting_dt = 0.1
-#         self.epsilon = 0.1
-#         self.max_steps = 10
-#         self.timestepper = AdaptiveTimestepper(self.u, self.v, self.h,
-#                                                self.timestep, self.starting_dt,
-#                                                epsilon=self.epsilon,
-#                                                max_steps=self.max_steps)
-
-#     def test_start_time(self):
-#         self.assertEqual(0, self.timestepper.time())
-
-#     def test_progress_with_low_error(self):
-#         steps, dt = self.timestepper.step_forwards()
-#         self.assertEqual(1, steps)
-#         self.assertEqual(self.starting_dt, dt)
-#         self.timestep.assert_has_calls([
-#             mock.call(mock.ANY, mock.ANY, mock.ANY, 0.1),
-#             mock.call(mock.ANY, mock.ANY, mock.ANY, 0.05),
-#             mock.call(mock.ANY, mock.ANY, mock.ANY, 0.05)])
-
-#     def test_timestepper(self):
-
-#         a  = AdaptiveTimestepper(u, v, h, timestep, starting_dt,
-#                                  epsilon=1)
-#         f0 = 0
-#         f = mock.Mock()
-#         a = AdaptiveTimestepper(f0, f, starting_h=0.1, epsilon=0.1)
-
-#         a.step_forwards()
-
 class TestStartingArrays(NumpyTestCase):
 
     def test_grid_shapes(self):
@@ -88,29 +51,31 @@ class TestCompute(NumpyTestCase):
         self.constants = SimpleNamespace(gravity=2.0, wind=1.0, dx=1.9, dy=1.9,
                                          drag=-1.5, h_background=100.2)
 
+    def compute_time_derivatives(self, u=np.zeros((6, 8)), v=np.zeros((7, 7)),
+                                 h=np.zeros((6, 7)), gravity=0.0, wind=0.0,
+                                 dx=1.0, dy=1.0, drag=0.0, h_background=1.0):
+        constants = SimpleNamespace(gravity=gravity, wind=wind, dx=dx, dy=dy,
+                                    drag=drag, h_background=h_background)
+        return compute_time_derivatives(u, v, h, constants)
+
     def assert_arrays_unchanged(self):
         self.assert_arrays_equal(self.rand_h_copy, self.rand_h)
         self.assert_arrays_equal(self.rand_u_copy, self.rand_u)
         self.assert_arrays_equal(self.rand_v_copy, self.rand_v)
 
-    # def test_values_of_du_dt(self):
-    #     u = np.arange(48).reshape(6, 8)
-    #     h = self.create_new_test_array()
-
-    #     constants = SimpleNamespace(gravity=1.0, wind=0.0, dx=1, dy=1,
-    #                                 drag=1.0, h_background=100.2)
-
-    #     du_dt, _, _ = compute_time_derivatives(u, self.rand_v, h,
-    #                                            self.constants)
-
-    #     expected = -np.array([[ 4,  6, -3,  6, 9, -1],
-    #                           [13,  3, 15, 15, 10, 14],
-    #                           [18, 15, 19, 21, 29, 14],
-    #                           [19, 25, 34, 20, 31, 32],
-    #                           [33, 40, 29, 40, 32, 43],
-    #                           [42, 39, 43, 51, 38, 53]])
-
-    #     self.assert_arrays_equal(du_dt, expected)
+    def test_dh_dx_term(self):
+        ''' Create a situation where du_dt = dh_dx * gravity, in order to
+        test this term '''
+        h = self.create_new_test_array()
+        gravity = 2
+        du_dt, _, _ = self.compute_time_derivatives(h=h, gravity=gravity)
+        expected = np.array([[ -3,  -4,   6,  -2,  -4,   7],
+                             [ -4,   7,  -4,  -3,   3,   0],
+                             [ -1,   3,   0,  -1,  -8,   8],
+                             [  6,   1,  -7,   8,  -2,  -2],
+                             [  0,  -6,   6,  -4,   5,  -5],
+                             [ -1,   3,   0,  -7,   7,  -7]]) * gravity
+        self.assert_arrays_equal(du_dt, expected)
 
     def test_compute_time_derivatives_does_not_change_input(self):
         compute_time_derivatives(self.rand_u, self.rand_v, self.rand_h,
